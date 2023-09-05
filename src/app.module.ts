f3fb96c1bc8498190680ea7eec45d21b8d587ff2
Otto Souza <ottosouza.dev@gmail.com>
2023-09-04 21:46:36 -0300

    start auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common'
-import { PrismaService } from './prisma/Prisma.service'
-import { CreateAccountController } from './controllers/create-account.controller'
-
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma/Prisma.service";
+import { CreateAccountController } from "./controllers/create-account.controller";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env";
+import { AuthModule } from "./auth/auth.module";
+import { AuthenticateController } from "./controllers/authenticate.controller";
 @Module({
-  imports: [],
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+
+    AuthModule,
+  ],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
